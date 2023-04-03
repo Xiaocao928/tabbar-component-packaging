3d38e587251810a6971bff47f10ae38dff236563
Xiaocao <caosinan1995@163.com>
2023-04-03 16:50:24 +0800

    1-完成tabbar组件的拆分

---
+++ src/components/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.tab-bar {
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 40px;
+  background-color: pink;
+}
+.tab-bar-item {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
